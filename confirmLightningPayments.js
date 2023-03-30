0f6be28cc4edfaba93d315ab6609bf9b8b8c722a
Olatunde Olatunde <olatunde.oladunni.dev@gmail.com>
2023-03-30 18:16:09 +0100

    Some new changes with lightning confirmation

--- confirmLightningPayments.js
+++ confirmLightningPayments.js
@@ -5,17 +5,21 @@ const axios = require('axios')
 const runLightningConfirm = async () => {
     console.log('starting')
     const pendingPaymentsList = await pendingPayments.find();
+            
+    console.log('await pendingPayments.find()', pendingPaymentsList.length);
+
     pendingPaymentsList.forEach(async (ment) => {
         const hash = ment.transactionId
         try {
             const checkStatus = await axios.get(`https://api.v4v.app/v1/check_invoice/${hash}`)
-            
             if (checkStatus.data && checkStatus.data.settled) {
                 const payment = ment.toObject();
                 delete payment['transactionId']
                 const donationObj = new donations(payment);
                 const savePayment = await donationObj.save();
-                console.log(savePayment)
+                console.log("savePayment")
+                await pendingPayments.deleteOne(ment.toObject());
+                console.log('ment deleted', pendingPaymentsList.length)
             }
         } catch (error) {
             //console.log(error)
